089cee25a2ceeca0a10207714bb12d623193029a
Marcos Colella <colella.dev@gmail.com>
2025-01-16 13:53:10 -0300

    Instalado Fonts e Loading

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 
 import theme from "./src/theme"
-
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+        { fontsLoaded ? <Home/> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
