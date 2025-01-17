84963b088ed80672104d84cabdb457227729f2af
Marcos Colella <colella.dev@gmail.com>
2025-01-17 14:01:13 -0300

    Estilizando Status Bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
+import { StatusBar } from 'react-native';
 
 import theme from "./src/theme"
 
@@ -12,6 +12,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+       <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
         { fontsLoaded ? <Home/> : <Loading /> }
     </ThemeProvider>
   );
