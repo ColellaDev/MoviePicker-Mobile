c665bf3f7f5a822f7ada8078905089c615f80ce6
Marcos Colella <colella.dev@gmail.com>
2025-01-25 16:27:36 -0300

    Implementado Navegação com BottomTabNavigator

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import theme from "./src/theme"
 
 import { Home } from './src/screens/Home';
+import { AppRoutes } from './src/routes/app.routes';
 import { Loading } from './src/Components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
+    <NavigationContainer>
     <ThemeProvider theme={theme}>
        <StatusBar 
           barStyle="light-content"
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <Home/> : <Loading /> }
+        { fontsLoaded ? <AppRoutes/> : <Loading /> }
     </ThemeProvider>
+    </NavigationContainer>
   );
 }
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,39 @@
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { Home } from '../screens/Home';
+import { Picker } from '../screens/Picker';
+import { Favorites } from '../screens/Favorites';
+import { MovieDetails } from '../screens/MovieDetails';
+
+type AppRoutes = {
+    home: undefined
+    picker: undefined
+    favorites: undefined
+    movieDetails: undefined
+  }
+  
+  export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
+  
+  const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
+
+export function AppRoutes() {
+    return (
+        <Navigator>
+            <Screen
+                name="home"
+                component={Home}
+            />
+            <Screen
+                name="picker"
+                component={Picker}
+            />
+            <Screen
+                name="favorites"
+                component={Favorites}
+            />
+            <Screen
+                name="movieDetails"
+                component={MovieDetails}
+            />
+        </Navigator>
+    )
+}
