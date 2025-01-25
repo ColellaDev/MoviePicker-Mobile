39eb2fe08a1a4ff555e5527719d41b34998cd562
Marcos Colella <colella.dev@gmail.com>
2025-01-25 17:07:10 -0300

    Estilização TabNavigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,6 +3,8 @@ import { Home } from '../screens/Home';
 import { Picker } from '../screens/Picker';
 import { Favorites } from '../screens/Favorites';
 import { MovieDetails } from '../screens/MovieDetails';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useTheme } from "styled-components/native";
 
 type AppRoutes = {
     home: undefined
@@ -16,26 +18,58 @@ type AppRoutes = {
   const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
+
+    const theme = useTheme();
+
     return (
         <Navigator screenOptions={{
             headerShown: false,
             tabBarShowLabel: false,
+            tabBarActiveTintColor: theme.COLORS.BASE_500,
+            tabBarInactiveTintColor: theme.COLORS.GRAY_300,
+            tabBarStyle: {
+                backgroundColor: theme.COLORS.GRAY_600,
+                borderTopWidth: 0,
+                paddingTop: 6,
+              },
+
         }}>
             <Screen
                 name="home"
                 component={Home}
+                options={{
+                    tabBarIcon: ({color}) => (
+                      <Ionicons name="home" size={30} color={color} />
+                    ),
+                  }}
             />
             <Screen
                 name="picker"
                 component={Picker}
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <Ionicons name="dice" size={30} color={color} />
+                    ),
+                  }}
             />
             <Screen
                 name="favorites"
                 component={Favorites}
+                options={{
+                    tabBarIcon: ({color}) => (
+                        <Ionicons name="heart" size={30} color={color} />
+                    ),
+                  }}
             />
             <Screen
                 name="movieDetails"
                 component={MovieDetails}
+                options={{ 
+                    tabBarButton: () => null,
+                    tabBarItemStyle: {
+                        display: 'none' 
+                      }
+                 }}
             />
         </Navigator>
     )
