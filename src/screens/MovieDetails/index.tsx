35e159885ead7f1c479b19494933c3eb44fc6e0e
Marcos Colella <colella.dev@gmail.com>
2025-01-26 19:56:34 -0300

    Arrumando Bug vote_avarage

--- src/screens/MovieDetails/index.tsx
+++ src/screens/MovieDetails/index.tsx
@@ -11,12 +11,14 @@ export function MovieDetails() {
     const route = useRoute();
     const { movie } = route.params as RouteParams;
 
+    const formattedVoteAverage  = Math.floor(movie.vote_average * 10) / 10;
+
     return (
         <Container>
             <Banner></Banner>
             <Poster source={{ uri: `https://image.tmdb.org/t/p/w300${movie.poster_path}` }} />  
             <Raiting>
-            <Ionicons name="star" size={13} color="#FFD447" />  {movie.vote_average}
+            <Ionicons name="star" size={13} color="#FFD447" />  {formattedVoteAverage}
             </Raiting>
             <Title>
                 {movie.title}
