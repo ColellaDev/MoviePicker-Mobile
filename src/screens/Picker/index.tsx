8b7797f807e1c8d22ea35f4528919edf2b12e35c
Marcos Colella <colella.dev@gmail.com>
2025-02-11 19:58:04 -0300

    Implementado função de sorteio e de balançar celular para sortear

--- src/screens/Picker/index.tsx
+++ src/screens/Picker/index.tsx
@@ -1,14 +1,68 @@
-import { Container} from "./styles";
+import { useState, useCallback } from "react";
+import { Alert, Vibration } from "react-native";
+import { Container, Text, DrawButton } from "./styles";
 import { useMovieContext } from "../../context/MovieContext";
-import { MovieList } from "../../Components/MovieList"; 
+import { MovieList } from "../../Components/MovieList";
+import { DeviceMotion } from "expo-sensors";
+import { useFocusEffect } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "../../routes/app.routes";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export function Picker() {
-
   const { picker } = useMovieContext();
+  const [lastMotion, setLastMotion] = useState<number>(0);
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  useFocusEffect(
+    useCallback(() => {
+      let subscription = DeviceMotion.addListener((motionData) => {
+        const { x, y, z } = motionData.accelerationIncludingGravity || {
+          x: 0,
+          y: 0,
+          z: 0,
+        };
+        const totalAcceleration = Math.abs(x) + Math.abs(y) + Math.abs(z);
+
+        if (totalAcceleration > 25 && Date.now() - lastMotion > 2000) {
+          setLastMotion(Date.now());
+          handleShake();
+        }
+      });
+
+      DeviceMotion.setUpdateInterval(200);
+
+      return () => subscription?.remove();
+    }, [picker, lastMotion])
+  );
+
+  const handleShake = () => {
+    if (picker.length === 0) {
+      Alert.alert("Lista vazia", "Adicione filmes antes de sortear.");
+      return;
+    }
+
+    Vibration.vibrate();
+
+    const randomIndex = Math.floor(Math.random() * picker.length);
+    const selectedMovie = picker[randomIndex];
+
+    navigation.navigate("movieDetails", { movie: selectedMovie });
+  };
 
   return (
     <Container>
-      <MovieList data={picker} emptyMessage={"Nenhum filme adicionado ao Picker"}/>
+      <Text>
+        Não sabe o que assistir? {"\n"} 
+        Balance o celular ou clique no dado e deixe o Picker decidir!
+      </Text>
+      <DrawButton onPress={handleShake}>
+        <Ionicons name="dice" size={24} color="#FFD447" />
+      </DrawButton>
+      <MovieList
+        data={picker}
+        emptyMessage={"Adicione filmes no Picker para sortear"}
+      />
     </Container>
   );
 }
--- src/screens/Picker/styles.ts
+++ src/screens/Picker/styles.ts
@@ -7,3 +7,18 @@ export const Container = styled.View`
   justify-content: center;
   padding-top: 50px;
 `;
+
+export  const Text = styled.Text`
+  color: ${({ theme }) => theme.COLORS.BASE_400};
+  text-align: center;
+  font-size: 18px;
+  margin-top: 5px;
+`
+
+export const DrawButton = styled.TouchableOpacity`
+  background-color: ${({theme}) =>  theme.COLORS.GRAY_400 };
+  padding: 4px 8px;
+  border-radius: 8px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`
